eac288ffa763acd98af1c789f923de839a1f9faa
Ivan Gavryliuk <ivan@isolineltd.com>
2020-01-23 12:50:16 +0000

    Redis early prototype (#17)
    * basic redis support, read strings and hashes
    
    * try to patch manifest
    
    * trying to put exe into artifact
    
    * trigger on redis ;)
    
    * fucking yaml
    
    * ps?
    
    * combine commands
    
    * combine commands
    
    * downgrade csv
    
    * combine commands
    
    * change versioning scheme
    
    * combine commands
    
    * uwp hw
    
    * uwp hw
    
    * uwp hw
    
    * version
    
    * another one
    
    * azcopy parameters
    
    * add RIs
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines

---
+++ src/StorAmp.Core/ViewModel/Redis/RedisHashEntryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace StorAmp.Core.ViewModel.Redis
+{
+   public class RedisHashEntryViewModel
+   {
+      public RedisHashEntryViewModel(HashEntry hashEntry)
+      {
+         Name = hashEntry.Name.ToString();
+         Value = hashEntry.Value.ToString();
+      }
+
+      public string Name { get; }
+      public string Value { get; }
+   }
+}
